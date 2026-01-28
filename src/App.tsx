1d8b28316c354be35250754b2b609fd270531461
DhruvS25 <dhruvsagre252@gmail.com>
2026-01-27 22:16:11 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import { BrowserRouter} from 'react-router-dom'
 import Index from './components/Index'
 import NotFound from './components/NotFound'
 
 function App() {
   return (
-    <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
-    >
-      <Routes>
-        <Route path="/" element={<Index />} />
+    <Routes>
+      <Route path="/" element={<Index />} />
 
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
